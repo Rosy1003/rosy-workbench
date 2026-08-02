1ba66ed4ec5e309a5f4131f3026d3bca27460687
Rosy1003 <1411210583@qq.com>
2026-08-02 13:41:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v11';
+const CACHE_NAME = 'rosy-deploy-v12';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
