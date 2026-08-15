509cb5285404d725253ccc6e93db6cc5227fd683
Rosy1003 <1411210583@qq.com>
2026-08-15 11:25:20 +0800

    feat: redesign Smart Assistant with workflow steps, metrics, categorized tasks - sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v35';
+const CACHE_NAME = 'rosy-deploy-v36';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
