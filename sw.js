ed668f75b837df0e3d400f6977b99d6f75cd4b3e
Rosy1003 <1411210583@qq.com>
2026-08-15 08:57:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v32';
+const CACHE_NAME = 'rosy-deploy-v33';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
