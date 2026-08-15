8739576dddec092d11cf305848473f189e44926f
Rosy1003 <1411210583@qq.com>
2026-08-15 12:54:14 +0800

    Add auto-cleanup of orphaned planShift on page load - restores hidden overdue tasks - sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v40';
+const CACHE_NAME = 'rosy-deploy-v41';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
