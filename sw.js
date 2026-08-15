a7782ea15080bfaa5827378b0bbcce911d41375f
Rosy1003 <1411210583@qq.com>
2026-08-15 10:48:07 +0800

    feat: Smart Assistant for overdue review task distribution - sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v33';
+const CACHE_NAME = 'rosy-deploy-v34';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
