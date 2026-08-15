aae05d32d21414d5cf61883aca0a04abe271a16a
Rosy1003 <1411210583@qq.com>
2026-08-15 12:30:16 +0800

    feat: today-due priority + new overdue days preview - sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rosy-deploy-v37';
+const CACHE_NAME = 'rosy-deploy-v38';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
